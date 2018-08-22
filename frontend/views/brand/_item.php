4f9faeb57974312aebb4af9c989d9db2b3d27bd0
S-fog <s-fog@yandex.ru>
2018-08-22 15:14:58 +0300

    update

--- frontend/views/brand/_item.php
+++ frontend/views/brand/_item.php
@@ -3,6 +3,6 @@ $filename = explode('.', basename($model->image));
 ?>
 <div class="brands__item">
     <a href="<?=$model->link?>" class="brands__itemImage"><img src="/images/thumbs/<?=$filename[0]?>-280-140.<?=$filename[1]?>" alt=""></a>
-    <div class="brands__itemText"><?=$model->name?></div>
+    <div class="brands__itemText"><?=$model->description?></div>
     <a href="<?=$model->link?>" class="brands__itemLink"><span>на страницу товаров бренда ––></span></a>
 </div>
