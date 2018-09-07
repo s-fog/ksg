2bf5e05b1414910b4baee9297f09c7a36d82eb2c
S-fog <s-fog@yandex.ru>
2018-09-07 19:57:30 +0300

    update

--- backend/views/yii2-app/layouts/header.php
+++ backend/views/yii2-app/layouts/header.php
@@ -11,6 +11,9 @@ use yii\helpers\Html;
         <span class="logo-lg">KSG</span>
     </a>
     <nav class="navbar navbar-static-top" role="navigation">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
         <div class="navbar-custom-menu">
 
             <ul class="nav navbar-nav">
