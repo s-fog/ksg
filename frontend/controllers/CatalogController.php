dc1c4506c8b5b6299ee8a1b438f8bcb784cdf3b6
S-fog <s-fog@yandex.ru>
2018-09-17 10:04:38 +0300

    update

--- frontend/controllers/CatalogController.php
+++ frontend/controllers/CatalogController.php
@@ -354,11 +354,11 @@ class CatalogController extends Controller
             var_dump($parent->aksses_ids);
             if (!empty($parent->aksses_ids)) {
                 $accessoriesCategory = Category::findOne(explode(',', $parent->aksses_ids));
-                $accessories = Product::find()
+                /*$accessories = Product::find()
                     ->where(['parent_id' => ArrayHelper::map($accessoriesCategory, 'id', 'id')])
                     ->orderBy(new Expression('rand()'))
                     ->limit(10)
-                    ->all();
+                    ->all();*/
             }
 
             return $this->render('view', [
