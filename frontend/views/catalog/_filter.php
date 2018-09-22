4ae2c7a4a6dfb4eba6aafebc88f1063e5cbe467a
S-fog <s-fog@yandex.ru>
2018-09-22 11:43:27 +0300

    update

--- frontend/views/catalog/_filter.php
+++ frontend/views/catalog/_filter.php
@@ -108,41 +108,43 @@ if ($model->type != 0) {
                         </div>
                     </div>
                 </div>
-                <div class="filter__item">
-                    <div class="filter__itemTop">
-                        <div class="filter__header filter__itemHeader"><em class="filter__headerMinus"></em><span>Характеристики</span></div>
-                        <div class="filter__description">{Ничего не выбрано}</div>
-                        <div class="filter__itemSearch">
-                            <input type="text" name="query" value="" autocomplete="off" class="filter__itemSearchInput" placeholder="найти в характеристиках">
-                            <div class="filter__itemSearchSubmit">x<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.77 30"><g><path d="M19.47,0a11.3,11.3,0,1,0,11.3,11.3A11.35,11.35,0,0,0,19.47,0Zm0,19.9A8.5,8.5,0,1,1,28,11.4,8.49,8.49,0,0,1,19.47,19.9Z"></path><path d="M19.47,4.4a1.37,1.37,0,0,0-1.4,1.4,1.37,1.37,0,0,0,1.4,1.4,4.23,4.23,0,0,1,4.2,4.2,1.41,1.41,0,0,0,2.81,0A7,7,0,0,0,19.47,4.4Z"></path><path d="M7.67,20.3.38,27.6a1.5,1.5,0,0,0,0,2,1.26,1.26,0,0,0,1,.4,1.28,1.28,0,0,0,1-.4l7.29-7.3a1.52,1.52,0,0,0,0-2A1.52,1.52,0,0,0,7.67,20.3Z"></path></g></svg></div>
+                <?php if (!empty($model->filterFeatures)) { ?>
+                    <div class="filter__item">
+                        <div class="filter__itemTop">
+                            <div class="filter__header filter__itemHeader"><em class="filter__headerMinus"></em><span>Характеристики</span></div>
+                            <div class="filter__description">{Ничего не выбрано}</div>
+                            <div class="filter__itemSearch">
+                                <input type="text" name="query" value="" autocomplete="off" class="filter__itemSearchInput" placeholder="найти в характеристиках">
+                                <div class="filter__itemSearchSubmit">x<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.77 30"><g><path d="M19.47,0a11.3,11.3,0,1,0,11.3,11.3A11.35,11.35,0,0,0,19.47,0Zm0,19.9A8.5,8.5,0,1,1,28,11.4,8.49,8.49,0,0,1,19.47,19.9Z"></path><path d="M19.47,4.4a1.37,1.37,0,0,0-1.4,1.4,1.37,1.37,0,0,0,1.4,1.4,4.23,4.23,0,0,1,4.2,4.2,1.41,1.41,0,0,0,2.81,0A7,7,0,0,0,19.47,4.4Z"></path><path d="M7.67,20.3.38,27.6a1.5,1.5,0,0,0,0,2,1.26,1.26,0,0,0,1,.4,1.28,1.28,0,0,0,1-.4l7.29-7.3a1.52,1.52,0,0,0,0-2A1.52,1.52,0,0,0,7.67,20.3Z"></path></g></svg></div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="filter__itemInner">
-                        <div class="filter__itemCategoriesContents">
-                            <div class="filter__itemCategoriesContent active">
-                            <?php foreach($model->filterFeatures as $index => $filterFeature) {
-                                $active = '';
-                                if ($index == 0) $active = ' active';
-                                ?>
-                                    <div class="filter__itemCategoriesContentItem">
-                                        <div class="filter__itemCategoriesContentHeader"><span><?=$filterFeature->name?></span></div>
-                                        <div class="filter__itemCategoriesContentList">
-                                            <?php foreach($filterFeature->filterFeatureValues as $filterFeatureValue) { ?>
-                                                <label class="filter__itemCategoriesContentListItem">
-                                                    <input type="checkbox"
-                                                           name="feature<?=$filterFeature->id?>_<?=$filterFeatureValue->id?>"
-                                                           <?=(isset($_GET["feature{$filterFeature->id}_{$filterFeatureValue->id}"])) ? ' checked': ''?>
-                                                           value="1">
-                                                    <span><?=$filterFeatureValue->name?></span>
-                                                </label>
-                                            <?php } ?>
+                        <div class="filter__itemInner">
+                            <div class="filter__itemCategoriesContents">
+                                <div class="filter__itemCategoriesContent active">
+                                <?php foreach($model->filterFeatures as $index => $filterFeature) {
+                                    $active = '';
+                                    if ($index == 0) $active = ' active';
+                                    ?>
+                                        <div class="filter__itemCategoriesContentItem">
+                                            <div class="filter__itemCategoriesContentHeader"><span><?=$filterFeature->name?></span></div>
+                                            <div class="filter__itemCategoriesContentList">
+                                                <?php foreach($filterFeature->filterFeatureValues as $filterFeatureValue) { ?>
+                                                    <label class="filter__itemCategoriesContentListItem">
+                                                        <input type="checkbox"
+                                                               name="feature<?=$filterFeature->id?>_<?=$filterFeatureValue->id?>"
+                                                               <?=(isset($_GET["feature{$filterFeature->id}_{$filterFeatureValue->id}"])) ? ' checked': ''?>
+                                                               value="1">
+                                                        <span><?=$filterFeatureValue->name?></span>
+                                                    </label>
+                                                <?php } ?>
+                                            </div>
                                         </div>
-                                    </div>
-                            <?php } ?>
+                                <?php } ?>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                <?php } ?>
                 <div class="filter__bottom">
                     <div class="filter__filterOff js-filter-close"><em class="filter__filterOffMinus"></em><span>свернуть фильтр</span></div>
                     <button type="submit" class="button button1 filter__submit">
