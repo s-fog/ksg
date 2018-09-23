c3f180cf8e03ae4f2c2f8254cdabfa28a23a277a
S-fog <s-fog@yandex.ru>
2018-09-23 22:05:21 +0300

    update

--- frontend/views/blocks/news.php
+++ frontend/views/blocks/news.php
@@ -10,8 +10,8 @@ $news = News::find()->limit(4)->orderBy(['created_at' => SORT_DESC])->all();
 ?>
 <div class="newsBlock">
     <div class="container">
-        <div class="newsBlock__header">свежие новости</div>
-        <a href="<?=Url::to(['site/index', 'alias' => $newsPage->alias])?>" class="newsBlock__text"><span>смотреть все новости</span></a>
+        <div class="newsBlock__header">Новое в нашем блоге</div>
+        <a href="<?=Url::to(['site/index', 'alias' => $newsPage->alias])?>" class="newsBlock__text"><span>Смотреть все статьи</span></a>
         <div class="newsBlock__inner">
             <?php foreach($news as $item) {
                 echo $this->render('@frontend/views/news/_item', [
