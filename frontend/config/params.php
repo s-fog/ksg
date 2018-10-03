5738854c0a124a0401fda2b15ceb0627bec9dc8b
S-fog <s-fog@yandex.ru>
2018-10-03 09:29:32 +0300

    update

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -11,8 +11,8 @@ return [
             'addressBrSchema' => '<span itemprop="postalCode">109004</span>, г. <span itemprop="addressLocality">Москва</span>,<br> ул. <span itemprop="streetAddress">Земляной вал, д. 64, стр. 2</span>',
         ],
         'Others' => [
-            'phone' => '+7 (495) 015-70-17',
-            'phoneLink' => '+74950157017',
+            'phone' => '8 (800) 350 06 08',
+            'phoneLink' => '+78003500608',
             'email' => 'hello@ksg.ru',
             'address' => '109004, г. Москва, ул. Земляной вал, д. 64, стр. 2',
             'addressBr' => '109004, г. Москва,<br> ул. Земляной вал, д. 64, стр. 2',
