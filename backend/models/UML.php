d90fffed5642f37beb231b382b9b023519d77a0c
S-fog <s-fog@yandex.ru>
2018-10-14 00:18:02 +0300

    update

--- backend/models/UML.php
+++ backend/models/UML.php
@@ -139,15 +139,19 @@ class UML extends Model
             $offer->appendChild($description);
 
             try {
+                $p = $product->variant->artikul;
+                /*
                 $vendorCode = $dom->createElement('vendorCode', $product->variant->artikul);
-                $offer->appendChild($vendorCode);
+                $offer->appendChild($vendorCode);*/
             } catch (Exception $e) {
 
             }
 
             try {
+                $p = $product->brand->name;
+                /*
                 $vendor = $dom->createElement('vendor', $product->brand->name);
-                $offer->appendChild($vendor);
+                $offer->appendChild($vendor);*/
             } catch (Exception $e) {
 
             }
