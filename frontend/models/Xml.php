8972620399fd3c38f48935f834429b98b983c3c7
S-fog <s-fog@yandex.ru>
2018-11-13 21:12:53 +0400

    update

--- frontend/models/Xml.php
+++ frontend/models/Xml.php
@@ -80,7 +80,7 @@ class Xml extends Model
                     if ($product->price > $item['price']) {
                         $str .= "attention;$artikul; Есть цена ниже у {$supplier->name}\r\n";
 
-                        Yii::$app->mailer->setFrom(Yii::$app->params['adminEmail'])
+                        Yii::$app->mailer_order->setFrom(Yii::$app->params['adminEmail'])
                             ->setTo('s-fog@yandex.ru')
                             ->setSubject("Для $artikul есть цена ниже у {$supplier->name}")
                             ->send();
@@ -89,7 +89,7 @@ class Xml extends Model
                     if ($available > 0 && $productParam->available == 0) {
                         $str .= "attention;$artikul; У {$supplier->name} товар есть в наличии\r\n";
 
-                        Yii::$app->mailer->setFrom(Yii::$app->params['adminEmail'])
+                        Yii::$app->mailer_order->setFrom(Yii::$app->params['adminEmail'])
                             ->setTo('s-fog@yandex.ru')
                             ->setSubject("Для $artikul у {$supplier->name} товар есть в наличии")
                             ->send();
