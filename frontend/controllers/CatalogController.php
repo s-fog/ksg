e74508b7ef0ebd44ea496ca6e6a8cb6200e7d132
S-fog <s-fog@yandex.ru>
2018-11-21 23:26:32 +0400

    update

--- frontend/controllers/CatalogController.php
+++ frontend/controllers/CatalogController.php
@@ -43,34 +43,6 @@ class CatalogController extends Controller
         } else {
             $model = Category::getCurrentCategory([$alias, $alias2, $alias3, $alias4, $alias5]);
 
-            if (!isset($_GET['page']) &&$model->type == 0 && $products = $cache->get('products-cat'.$model->id)) {
-                $pages = $cache->get('pages-cat'.$model->id);
-                $tags = $cache->get('tags-cat'.$model->id);
-                $brands = $cache->get('brands-cat'.$model->id);
-                $years = $cache->get('years-cat'.$model->id);
-                $brandsSerial = $cache->get('brandsSerial-cat'.$model->id);
-                $bHeader = $cache->get('bHeader-cat'.$model->id);
-                $bHeader2 = $cache->get('bHeader2-cat'.$model->id);
-                $minPrice = $cache->get('minPrice-cat'.$model->id);
-                $maxPrice = $cache->get('maxPrice-cat'.$model->id);
-                $filterBrands = $cache->get('filterBrands-cat'.$model->id);
-
-                return $this->render('index', [
-                    'model' => $model,
-                    'products' => $products,
-                    'pages' => $pages,
-                    'tags' => $tags,
-                    'brands' => $brands,
-                    'years' => $years,
-                    'brandsSerial' => $brandsSerial,
-                    'bHeader' => $bHeader,
-                    'bHeader2' => $bHeader2,
-                    'minPrice' => $minPrice,
-                    'maxPrice' => $maxPrice,
-                    'filterBrands' => $filterBrands,
-                ]);
-            }
-
             $innerIdsWhere = [];
 
             if (!$model) {
@@ -398,20 +370,6 @@ class CatalogController extends Controller
                 if ($i >= $pages->offset) {
                     $products[] = $allproducts[$i];
                 }
-            }
-
-            if (!isset($_GET['page']) &&$model->type == 0) {
-                $cache->set('products-cat'.$model->id, $products);
-                $cache->set('pages-cat'.$model->id, $pages);
-                $cache->set('tags-cat'.$model->id, $tags);
-                $cache->set('brands-cat'.$model->id, $brands);
-                $cache->set('years-cat'.$model->id, $years);
-                $cache->set('brandsSerial-cat'.$model->id, $brandsSerial);
-                $cache->set('bHeader-cat'.$model->id, $bHeader);
-                $cache->set('bHeader2-cat'.$model->id, $bHeader2);
-                $cache->set('minPrice-cat'.$model->id, $minPrice);
-                $cache->set('maxPrice-cat'.$model->id, $maxPrice);
-                $cache->set('filterBrands-cat'.$model->id, $filterBrands);
             }
 
             return $this->render('index', [
