bbad0f4b69d0bb1ec0bdc03cd3a664f9e46adb81
S-fog <s-fog@yandex.ru>
2018-11-22 01:26:26 +0400

    update

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
-var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
@@ -9,13 +8,17 @@ var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
-const pngquant = require('imagemin-pngquant');
 var plumber = require('gulp-plumber');
+var imageminJpegRecompress = require('imagemin-jpeg-recompress');
+var imageminPngquant = require('imagemin-pngquant');
+var cacheFiles = require('gulp-cache-files');
+const webp = require('gulp-webp');
+const clone = require('gulp-clone');
+const clonesink = clone.sink();
 var autoprefixerOptions = {
     browsers: ['> 1%'],
     cascade: false
 };
-var cssDest = 'assets/css';
 
 gulp.task('browser-sync', function () {
     browserSync({
@@ -23,6 +26,71 @@ gulp.task('browser-sync', function () {
             baseDir: '.'
         }
     });
+});
+
+gulp.task('images', function () {
+    return gulp.src('img-full/**/*.{jpg,png}', {read: false})
+        .pipe(cacheFiles.filter('manifest.json'))
+        .pipe(plumber())
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imageminJpegRecompress({
+                progressive: true,
+                max: 80,
+                min: 70
+            }),
+            imageminPngquant({quality: '80'}),
+            imagemin.optipng({optimizationLevel: 4}),
+            imagemin.svgo()
+        ]))
+        .pipe(cacheFiles.manifest())
+        .pipe(clonesink) // start stream
+        .pipe(webp({quality: '80'})) // convert images to webp and save a copy of the original format
+        .pipe(clonesink.tap()) // close stream and send both formats to dist
+        .pipe(gulp.dest('img'));
+});
+
+gulp.task('images-uploads', function () {
+    return gulp.src('uploads/*')
+        .pipe(plumber())
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imageminJpegRecompress({
+                progressive: true,
+                max: 80,
+                min: 70
+            }),
+            imageminPngquant({quality: '80'}),
+            imagemin.optipng({optimizationLevel: 4}),
+            imagemin.svgo()
+        ]))
+        .pipe(clonesink) // start stream
+        .pipe(webp({quality: '80'})) // convert images to webp and save a copy of the original format
+        .pipe(clonesink.tap()) // close stream and send both formats to dist
+        .pipe(gulp.dest('uploads'));
+});
+
+gulp.task('images-thumbs', function () {
+    return gulp.src('images/thumbs/*')
+        .pipe(plumber())
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imageminJpegRecompress({
+                progressive: true,
+                max: 80,
+                min: 70
+            }),
+            imageminPngquant({quality: '80'}),
+            imagemin.optipng({optimizationLevel: 4}),
+            imagemin.svgo()
+        ]))
+        .pipe(clonesink) // start stream
+        .pipe(webp({quality: '80'})) // convert images to webp and save a copy of the original format
+        .pipe(clonesink.tap()) // close stream and send both formats to dist
+        .pipe(gulp.dest('images/thumbs'));
 });
 
 gulp.task('scripts', function () {
@@ -41,7 +109,7 @@ gulp.task('scripts.vendor', function () {
     return gulp.src([
         'js/vendor/jquery-ui/jquery-ui.min.js',
         'js/vendor/fancybox/dist/jquery.fancybox.js',
-        'js/vendor/owl.carousel/dist/owl.carousel.js'
+        'js/vendor/owl.carousel/dist/owl.carousel.min.js'
         ])
         .pipe(concat('vendor.js'))
         .pipe(plumber())
@@ -61,7 +129,8 @@ gulp.task('styles', function () {
 
 gulp.task('build', ['scripts', 'scripts.vendor', 'styles', 'images']);
 
-gulp.task('watch', ['scripts', 'styles'], function () {
+gulp.task('watch', ['browser-sync', 'scripts', 'styles', 'images'], function () {
+    gulp.watch('img-full/**/*.{jpg,png}', ['images']);
     gulp.watch('js/script.es6', ['scripts']);
     gulp.watch('css/**/*.scss', ['styles']);
     gulp.watch('*.php', browserSync.reload);
