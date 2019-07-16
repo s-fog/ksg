42d250a4770589365b398c763c38a4bfd5d6fcaf
S-fog <s-fog@yandex.ru>
2019-07-16 19:09:57 +0400

    update

--- backend/models/ChangerForm.php
+++ backend/models/ChangerForm.php
@@ -42,7 +42,7 @@ class ChangerForm extends \yii\db\ActiveRecord
     public function changePrices() {
         $products = Product::find()
             ->joinWith(['supplier', 'brand'])
-            ->andWhere(['>=', Changer::tableName().'.updated_at', time() - 172800])
+            ->andWhere(['>=', Product::tableName().'.updated_at', time() - 172800])
             ->andWhere(['>=', 'price', $this->price_from])
             ->andWhere(['<=', 'price', $this->price_to]);
 
