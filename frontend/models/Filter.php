a1826ff24e3c667568a913f19a1bceac61a1408e
Sergei Monakov <s-fog@yandex.ru>
2019-09-13 09:51:14 +0300

    update

--- frontend/models/Filter.php
+++ frontend/models/Filter.php
@@ -48,7 +48,7 @@ class Filter
                 ->groupBy('product_id')
                 ->having(['c' => count($filterFeaturesValue)]);
             if (isset($_GET['gg'])) {
-                var_dump($fQuery->asArray()->all());die();
+                var_dump(ArrayHelper::getColumn($fQuery->asArray()->all(), 'product_id'));die();
             }
             $query->andWhere([Product::tableName().'.id' => ArrayHelper::getColumn($fQuery->asArray()->all(), 'product_id')]);
         }
