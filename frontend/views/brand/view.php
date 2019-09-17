6596dd926ab79020a60a1b5295987b8406c983d1
Sergei Monakov <s-fog@yandex.ru>
2019-09-17 13:12:52 +0300

    update

--- frontend/views/brand/view.php
+++ frontend/views/brand/view.php
@@ -32,59 +32,72 @@ $presents = \common\models\Present::find()->all();
 
 <?=$this->render('@frontend/views/blocks/breadcrumbs', ['items' => $model->getBreadcrumbs()])?>
 
-<?=$this->render('@frontend/views/catalog/_filterTop', [
-    'model' => $model,
-    'minPrice' => $minPrice,
-    'maxPrice' => $maxPrice,
-    'filterBrands' => $filterBrands,
-])?>
-
-<div class="infs">
-    <div class="container">
-        <div class="infs__header"><h1><?=$h1?></h1><?=($page == 1) ? "<span>({$model->productCount})</span>" : ''?></div>
-        <?php if ($page == 1) {  ?>
-            <p class="infs__text">
-                В каталоге представлены товары бренда <?=$model->name?> по цене от <?=number_format($minPrice, 0, '', ' ')?> рублей! Интернет-магазин KSG - официальный дилер <?=$model->name?> в России, покупая у нас вы получаете фирменную гарантию от производителя. Доставка по Москве и регионам РФ.
-            </p>
-        <?php }  ?>
+    <div class="catalogTop">
+        <div class="container">
+            <div class="catalogTop__inner<?=empty($childrenCategories) ? ' catalogTop__inner_categoriesEmpty' : ''?>">
+                <div class="catalogTop__h1<?=empty($childrenCategories) ? ' catalogTop__h1_categoriesEmpty' : ''?>">
+                    <h1><?=$h1?></h1>
+                    <?=($page == 1) ? "<span>({$model->productCount})</span>" : ''?>
+                </div>
+                <select class="catalogTop__sort sort-select" name="sort">
+                    <?php if (($model::className() == 'common\models\Brand') ||
+                        ($model::className() == 'common\models\Category' && ($model->level === 3 || in_array($model->type, [1, 2, 3, 4])))) { ?>
+                        <option value="popular_desc"<?=(isset($_GET['sort']) && $_GET['sort'] == 'popular_desc') ? ' selected' : ''?>>
+                            по популярности</option>
+                        <option value="price_asc"<?=((isset($_GET['sort']) && $_GET['sort'] == 'price_asc') || !isset($_GET['sort'])) ? ' selected' : ''?>>
+                            сначала дешевле</option>
+                        <option value="price_desc"<?=(isset($_GET['sort']) && $_GET['sort'] == 'price_desc') ? ' selected' : ''?>>
+                            сначала дороже</option>
+                    <?php } else { ?>
+                        <option value="popular_desc"<?=((isset($_GET['sort']) && $_GET['sort'] == 'popular_desc') || !isset($_GET['sort'])) ? ' selected' : ''?>>
+                            по популярности</option>
+                        <option value="price_asc"<?=(isset($_GET['sort']) && $_GET['sort'] == 'price_asc') ? ' selected' : ''?>>
+                            сначала дешевле</option>
+                        <option value="price_desc"<?=(isset($_GET['sort']) && $_GET['sort'] == 'price_desc') ? ' selected' : ''?>>
+                            сначала дороже</option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
     </div>
-    <?=$this->render('@frontend/views/catalog/_filter', [
-        'model' => $model,
-        'minPrice' => $minPrice,
-        'maxPrice' => $maxPrice,
-        'filterBrands' => $filterBrands,
-        'filterFeatures' => $filterFeatures,
-    ])?>
-</div>
+    <div class="catalog">
+        <div class="container">
+            <div class="catalog__inner">
+                <div class="catalog__innerLeft">
+                    <?=$this->render('@frontend/views/catalog/_filter', [
+                        'model' => $model,
+                        'minPrice' => $minPrice,
+                        'maxPrice' => $maxPrice,
+                        'filterBrands' => $filterBrands,
+                        'childrenCategories' => false,
+                        'filterFeatures' => $filterFeatures,
+                    ])?>
+                </div>
+                <div class="catalog__innerRight">
+                    <div class="catalog__innerRightItems">
+                    <?php
+                    foreach($products as $index => $item) {
+                        echo $this->render('@frontend/views/catalog/_item', [
+                            'model' => $item
+                        ]);
+                    } ?>
+                </div>
+            </div>
 
-<?=$this->render('@frontend/views/blocks/cats', [
-    'inCategories' => $inCategories,
-    'childrenCategories' => [],
-])?>
-
-<div class="catalog">
-    <div class="container">
-        <div class="catalog__inner">
-            <?php
-            foreach($products as $index => $item) {
-                echo $this->render('@frontend/views/catalog/_item', [
-                    'model' => $item
-                ]);
-            } ?>
+                <?=LinkPager::widget([
+                    'pagination' => $pages,
+                    'disableCurrentPageButton' => true,
+                    'hideOnSinglePage' => true,
+                    'maxButtonCount' => 6,
+                    'nextPageLabel' => '&gt;',
+                    'prevPageLabel' => '&lt;',
+                    'firstPageLabel' => '&lt;&lt;',
+                    'lastPageLabel'  => '&gt;&gt;'
+                ]);?>
         </div>
     </div>
 </div>
 
-<?=LinkPager::widget([
-    'pagination' => $pages,
-    'disableCurrentPageButton' => true,
-    'hideOnSinglePage' => true,
-    'maxButtonCount' => 6,
-    'nextPageLabel' => '&gt;',
-    'prevPageLabel' => '&lt;',
-    'firstPageLabel' => '&lt;&lt;',
-    'lastPageLabel'  => '&gt;&gt;'
-]);?>
 
 <?php foreach($products as $product) {
     echo $this->render('@frontend/views/catalog/_addToCart_items', [
