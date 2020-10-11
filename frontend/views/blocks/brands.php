d99389d80514145b57125b2a5cf92f2b0a3932cb
s-fog <s-fog@yandex.ru>
2020-10-11 10:59:00 +0300

    f

--- frontend/views/blocks/brands.php
+++ frontend/views/blocks/brands.php
@@ -5,7 +5,7 @@ use yii\helpers\Url;
 ?>
 <div class="brands">
     <div class="container">
-        <div class="brands__header">бренды-партёры KSG</div>
+        <div class="brands__header">бренды-партнёры KSG</div>
         <a href="<?=Url::to(['site/index', 'alias' => Textpage::findOne(2)->alias])?>" class="brands__link linkSpan"><span>смотреть все бренды</span></a>
         <div class="brands__inner owl-carousel">
             <?php foreach(Brand::find()->orderBy(['sort_order' => SORT_DESC])->limit(15)->all() as $item) {
