6c991f70d82a9056cdd87c1e4473b43dfad22b9d
s-fog <s-fog@yandex.ru>
2021-02-04 15:39:46 +0300

    f

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -2,16 +2,16 @@
 return [
     'cities' => [
         'Москва' => [
-            'phone' => '+7 (495) 015-70-17',
-            'phoneLink' => '+74950157017',
+            'phone' => '+7 (495) 150 21 48',
+            'phoneLink' => '+74951502148',
             'email' => 'hi@ksg.ru',
             'address' => '109004, г. Москва, ул. Земляной вал, д. 64, стр. 2',
             'addressBr' => '109004, г. Москва,<br> ул. Земляной вал, д. 64, стр. 2',
             'addressBrSchema' => '<span itemprop="postalCode">109004</span>, г. <span itemprop="addressLocality">Москва</span>,<br> ул. <span itemprop="streetAddress">Земляной вал, д. 64, стр. 2</span>',
         ],
         'Others' => [
-            'phone' => '8 (800) 350 06 08',
-            'phoneLink' => '+78003500608',
+            'phone' => '8 (800) 333 07 18',
+            'phoneLink' => '+78003330718',
             'email' => 'hi@ksg.ru',
             'address' => '109004, г. Москва, ул. Земляной вал, д. 64, стр. 2',
             'addressBr' => '109004, г. Москва,<br> ул. Земляной вал, д. 64, стр. 2',
