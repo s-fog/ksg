2d508c83088670c4cc9e2adb874567df4b86e069
s-fog <s-fog@yandex.ru>
2021-02-07 20:32:35 +0300

    f

--- frontend/models/Xml.php
+++ frontend/models/Xml.php
@@ -43,6 +43,7 @@ class Xml extends Model
                 if ($productParam = ProductParam::findOne(['artikul' => $artikul])) {
                     $message = [];
                     $product = Product::findOne($productParam->product_id);
+                    if (!$product) continue;
                     $old_price = $product->price;
                     
                     if (
