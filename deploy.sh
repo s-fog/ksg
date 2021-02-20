5c1608b3903c850928e3fb16686faac3066d4006
s-fog <s-fog@yandex.ru>
2021-02-20 09:18:55 +0300

    f

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 ssh -i "~/.ssh/ksg_rsa" ubuntu@46.229.214.117 <<'ENDSSH'
 cd /var/www/ksg.ru
 git pull
-sudo systemctl restart ksg-queue.service
+sudo systemctl restart ksg-queue.target
 docker-compose run --rm ksg_php ./deploy_php.sh
 ENDSSH
